756af405dcd42e116161f91e1f81fd1de7aaa247
Puneet017 <95585582+Puneet017@users.noreply.github.com>
2021-12-07 01:45:27 +0530

    created postivenum.py
    this file prints the positive numbers from the the given lists.

---
+++ positivenum.py
@@ -0,0 +1,21 @@
+#input of list1
+list1 =[12,-7,5,64,-14]
+print("positive numbers in list 1:\n")
+for num in list1:
+      
+    # checking condition
+    if num >= 0:
+       print(num, end ="\n ")
+
+
+
+#input of list2
+list2= [12,14,-95,3]
+print("positive numbers in list 2:\n")
+for num in list2:
+
+    #checking condition
+    if num >= 0:
+       print(num, end = "\n ")
+      
+      
